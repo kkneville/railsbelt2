256fa11ef57a04522fceb625dcffc29606bb3093
kkneville <mskatherinekrueger@gmail.com>
2017-10-21 12:17:24 -0500

    last changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
       end 
     else
       flash[:errors] = ["Email and password do not match"]
-      return redirect_to '/login'
+      return redirect_to login_page_path
     end  
   end
 
